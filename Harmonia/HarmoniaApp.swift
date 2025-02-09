d5030d247b5db55aecdbbf37c63ec90c4a22b650
Rivaldo Fernandes <rivaldofez@gmail.com>
2025-02-09 17:23:11 +0700

    [FEAT] Setup base project and Github Actions

---
+++ Harmonia/HarmoniaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarmoniaApp.swift
+//  Harmonia
+//
+//  Created by Rivaldo Fernandes on 09/02/25.
+//
+
+import SwiftUI
+
+@main
+struct HarmoniaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
