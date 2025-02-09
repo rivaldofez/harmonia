c96b4e28bb1b627c50996c3a150699e198f79135
Rivaldo Fernandes <rivaldofez@gmail.com>
2025-02-09 20:22:01 +0700

    [FEAT] Create enum for view state

---
+++ Harmonia/Core/Component/ViewState.swift
@@ -0,0 +1,14 @@
+//
+//  ViewState.swift
+//  Harmonia
+//
+//  Created by Rivaldo Fernandes on 09/02/25.
+//
+
+import Foundation
+
+enum ViewState {
+    case loading
+    case error(message: String)
+    case success
+}
